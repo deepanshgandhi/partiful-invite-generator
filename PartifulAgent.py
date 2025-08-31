4a2cf4cf8de8df861a58ec8a73641a395c4a7abf
Yeabkal Abeje <yeabkal@dhcp-10-29-214-147.dyn.MIT.EDU>
2025-08-31 14:55:07 -0400

    Base Structure Outline

---
+++ PartifulAgent.py
@@ -0,0 +1,9 @@
+"""
+
+We want the agent to do the following: 
+ - Login using the HOST_NUMBER in .env
+ - Bypass verification or hopefully it is on a device where the lgoin is cached so it shouldn't need to 
+ - Go to https://partiful.com/create
+ - Based on the pydantic information provided fill in the appropriate inputs
+
+"""
---
+++ TextToStructuredData.py
@@ -0,0 +1,9 @@
+
+"""
+Given some string turn it into a pydantic text
+
+"""
+
+def text_to_structured(event_details : str): 
+    #fill in stuff from Deepansh
+    pass
